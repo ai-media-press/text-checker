8475fcb4b94ca2ded1afa05943c5e2386e29d89a
ai-media-press <ai.media.press@gmail.com>
2024-07-09 13:40:10 +0000

    initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 import streamlit as st
+import re
+from typing import List
+from collections import Counter
+from math import log
+import textstat
+import tiktoken
+text = st.text_area("text")
+calculate = st.button("Calculate")
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+
+def shannon(string):
+    counts = Counter(string)
+    frequencies = ((i / len(string)) for i in counts.values())
+    return - sum(f * log(f, 2) for f in frequencies)
+
+
+
+if calculate:
+    st.text(f"Shannon entropy: {shannon(text):.2f}")
+    st.text(f"Gunning fox index: {textstat.gunning_fog(text)}")
+    st.text(f"No of tokens: {len(encoding.encode(text))}")
